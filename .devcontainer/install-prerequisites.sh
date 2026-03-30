c61f675e062e3805eedec574aea522ddf6b62f69
Pablo Zaidenvoren <pablozaiden@users.noreply.github.com>
2026-03-30 12:01:03 +0000

    node

--- .devcontainer/install-prerequisites.sh
+++ .devcontainer/install-prerequisites.sh
@@ -2,7 +2,17 @@
 
 set -e
 
+# support executing node scripts
+if [ ! -f "/usr/localbin/node" ]; then
+    sudo ln -s /usr/local/bin/bun /usr/local/bin/node
+fi
+
 # install sshpass and dtach
 sudo apt-get update && sudo apt-get install -y sshpass dtach
 
+# install playwright
 bunx playwright install --with-deps --only-shell
+
+# install playwright cli
+sudo bun install -g @playwright/cli@latest
+
